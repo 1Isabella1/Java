1175f93f57939dc872efd4648db5da5fa061874d
Isa Dev <isabella.salcedo.engineer@gmail.com>
2024-10-25 17:21:19 -0500

    Nuevo módulo de curso

---
+++ Cadenas/src/ManejoSubcadenas.java
@@ -0,0 +1,2 @@
+public class ManejoSubcadenas {
+}
--- Cadenas/src/MetodosDeCadenas.java
+++ Cadenas/src/MetodosDeCadenas.java
@@ -10,5 +10,17 @@ public class MetodosDeCadenas {
         //Reemplazar caracteres
         var nuevaCadena = cadena1.replace('o', 'a');
         System.out.println("nuevaCadena = " + nuevaCadena);
+
+        //Convertir a mayusculas
+        var mayusculas = cadena1.toUpperCase();
+        System.out.println("mayusculas = " + mayusculas);
+
+        //Convertir a minusculas
+        System.out.println("minusculas = " + cadena1.toLowerCase());
+
+        //Eliminar espacios al inicio y al final
+        var cadena2 = " Leo Reyes    ";
+        System.out.println("cadena2 = " + cadena2);
+        System.out.println("cadena2 sin espacios = " + cadena2.trim());
     }
 }
